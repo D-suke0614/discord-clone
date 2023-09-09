db51a94d36735f16363976aeff4e7d6160466323
Daisuke <d.suke.kida@gmail.com>
2023-09-09 18:09:57 +0900

    firebaseとreduxを用いたログイン機能の実装

---
+++ src/firebase.ts
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore/lite"
+import { GoogleAuthProvider, getAuth } from "firebase/auth"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC4_OuHsJVe3JBGdzIdhXUagIrZnVfJ16M",
+  authDomain: "discord-clone-32a78.firebaseapp.com",
+  projectId: "discord-clone-32a78",
+  storageBucket: "discord-clone-32a78.appspot.com",
+  messagingSenderId: "608251160940",
+  appId: "1:608251160940:web:c9919d77eb73277b052b49"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+const auth = getAuth(app)
+const provider = new GoogleAuthProvider()
+
+export { auth, provider, db }
